d1eee46f12adcd9216973e9db035264a6d929d8b
Md Imam Hussain <mihussain737@gmail.com>
2025-11-10 21:23:47 +0530

    expressiong

--- first react app/src/App.jsx
+++ first react app/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 
 function App() {
-  const element =<h1>Hello World</h1>
-  return (
+  const name="Alice";
+  const btnlabel="Click Me";
+    return (
     <>
-    {element}
+    <h1 className='greeting'>Hello, {name}</h1>
+    <button style={{background:'green',color:'white'}}>{btnlabel}</button>
+    <button onClick={()=>alert(`Button Clicked`)}>Alert Me</button>
+    <img src='https://placehold.co/100x100'></img>
     </>
   )
 }
