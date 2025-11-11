34be56cceb67f64d735f5fcd9727c4ffc854d28b
Md Imam Hussain <mihussain737@gmail.com>
2025-11-11 09:13:03 +0530

    comments added

--- first react app/src/App.jsx
+++ first react app/src/App.jsx
@@ -1,16 +1,21 @@
 import './App.css'
 
 function App() {
-  const name="Alice";
-  const btnlabel="Click Me";
-    return (
-    <>
-    <h1 className='greeting'>Hello, {name}</h1>
-    <button style={{background:'green',color:'white'}}>{btnlabel}</button>
-    <button onClick={()=>alert(`Button Clicked`)}>Alert Me</button>
-    <img src='https://placehold.co/100x100'></img>
-    </>
-  )
-}
+  // this is single line comments
 
-export default App
+  /*
+    this is multi line comment
+  */
+
+  return(
+    <div>
+      {/* This is singl line comment in jsx code/}
+
+      {/* This is multi line comments in jsx code
+      
+      */}
+    </div>
+  );
+}
+export default App;
+ 
