cefa64c9dce1ed94d6a59dc6c1aa775665f6a378
Md Imam Hussain <mihussain737@gmail.com>
2025-11-11 09:42:45 +0530

    portfolio project

--- first react app/src/App.jsx
+++ first react app/src/App.jsx
@@ -1,20 +1,53 @@
 import './App.css'
 
 function App() {
-  // this is single line comments
-
-  /*
-    this is multi line comment
-  */
+  const name="Md Imam Hussain";
+  const profession="Full Stack Developer";
+  const projects=[
+    {
+      title:"Project One",
+      description:"A web application built using React and Node js",
+      link:"#"
+    },
+     {
+      title:"Project Two",
+      description:"A Ecommerce application built using React and Node js",
+      link:"#"
+    }
+  ]
 
   return(
-    <div>
-      {/* This is singl line comment in jsx code/}
+   <div className='App'>
+    {/* Header Section */}
+      <header className='header'>
+        <h1>{name}</h1>
+        <p>{profession}</p>
+        <nav>
+          <a href='#about'>About</a>
+          <a href='#about'>Projects</a>
+          <a href='#about'>Contacts</a>
+        </nav>
+      </header>
 
-      {/* This is multi line comments in jsx code
-      
-      */}
-    </div>
+      {/* About Section */}
+      <section id="about" className='about-section'>
+        <h2>About Me</h2>
+        <p>Hello! I am {name}, A passionate {profession}. I love building web application and solve real time problems.</p>
+      </section>
+
+      {/* Project Section */}
+      <section id="projects" className='projects-section'>
+        <h2>Projects</h2>
+        <div className='projects-list'>
+          {projects.map((project,index)=>(
+            <div key={index} className='project-item'>
+              <h3>{project.title}</h3>
+              <h3>{project.description}</h3>
+            </div>
+          ))}
+        </div>
+      </section>
+   </div>
   );
 }
 export default App;
