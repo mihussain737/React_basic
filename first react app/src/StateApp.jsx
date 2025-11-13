3e14cc551b9469b70ca5c65c04a579ad2d1863b9
Md Imam Hussain <mihussain737@gmail.com>
2025-11-13 20:38:42 +0530

    arrays and objects added with counter projects

---
+++ first react app/src/StateApp.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './StateApp.css';
+
+function StateApp(){
+     const [counters,setCounters]=useState([{id:1, value:0}]);
+
+     const addCounter=()=>{
+          setCounters([...counters,{id:counters.length+1, value:0}]);
+     };
+
+     const incrementCounter=(id)=>{
+          setCounters(counters.map(counter=>
+               counter.id===id ? {...counter, value:counter.value+1} : counter
+          )
+     );
+     };
+return(
+     <div className='app-container'>
+          <button onClick={addCounter}>Add Counter</button>
+
+          <ul>
+               {counters.map(counter=>(
+                    <li key={counter.id}>
+                             Counter {counter.id} : {counter.value}
+                    <button onClick={()=>incrementCounter(counter.id)}>Increment</button>
+                    </li>
+               ))}
+          </ul>
+     </div>
+)
+}
+
+export default StateApp;
