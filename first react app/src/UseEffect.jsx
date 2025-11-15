853f3eafcb635f9a47b37ba1fb57474390a4ad62
Md Imam Hussain <mihussain737@gmail.com>
2025-11-15 10:29:41 +0530

    arrays dependency on useEffect

--- first react app/src/UseEffect.jsx
+++ first react app/src/UseEffect.jsx
@@ -3,18 +3,29 @@ import './UseEffect.css';
 
 const UseEffect = () => {
      const  [count,setcount]=useState(0);
+     const [anotherVale,setAnotherValue]=useState(10);
 
      useEffect(()=>{
           document.title=`Count: ${count +1}`;
+          console.log("use effect triggered");
      },[count])
+
+     useEffect(()=>{
+          document.title=`Count: ${count +1}`;
+          console.log("use effect triggered");
+     })
 
      const incrementCount=()=>{
           setcount(count+1);
-          // document.title=`Count: ${count +1}`;
+     }
+     const incrementValue=()=>{
+          setAnotherValue(anotherVale+1)
      }
   return (
     <div><h1>UseEffect</h1>
-    <button onClick={incrementCount}>Increment</button></div>
+    <button onClick={incrementCount}>Increment</button>
+    <button onClick={incrementValue}>Another Value</button>
+    </div>
   )
 }
 
