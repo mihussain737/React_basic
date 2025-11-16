fdc96a70ef9cbe6e18c5b7999a951f0c3fa83506
Md Imam Hussain <mihussain737@gmail.com>
2025-11-16 09:24:28 +0530

    mouse tracker project added

--- project-1/src/main.jsx
+++ project-1/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Project2 from './Project2.jsx'
+import MouseTracker from './MouseTracker.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Project2/>
+    {/* <Project2/> */}
+    <MouseTracker></MouseTracker>
   </StrictMode>,
 )
