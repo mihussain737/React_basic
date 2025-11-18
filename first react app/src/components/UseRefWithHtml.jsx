b2e9b384f520089da5f3e51eaa751f7eae5ae535
Md Imam Hussain <mihussain737@gmail.com>
2025-11-18 21:26:24 +0530

    use ref reset

--- first react app/src/components/UseRefWithHtml.jsx
+++ first react app/src/components/UseRefWithHtml.jsx
@@ -13,13 +13,18 @@ const UseRefWithHtml = () => {
           inputRef.current.focus();
           inputRef.current.style.backgroundColor="yellow";
      };
+
+     const resetFocus=()=>{
+          inputRef.current.style.backgroundColor="white";
+     }
   return (
     <div>
      <h1>Learn React</h1>
      <input type="text" ref={inputRef} placeholder='Focus me' />
      <br />
      <button onClick={focusInput}>Focus</button>
-     <button onClick={focusInputHighlight}>Focus and highlight</button>
+     <button onClick={focusInputHighlight}>Focus and highlight</button><br />
+     <button onClick={resetFocus}>Reset</button>
     </div>
   )
 }
