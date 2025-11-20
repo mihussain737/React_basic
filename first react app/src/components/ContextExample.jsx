8169e85533ac9c1518a437ac4396600d052232d5
Md Imam Hussain <mihussain737@gmail.com>
2025-11-20 20:27:24 +0530

    updating value of context using state

--- first react app/src/components/ContextExample.jsx
+++ first react app/src/components/ContextExample.jsx
@@ -1,12 +1,19 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 const ThemeContext = createContext("light");
 
 const ContextExample = () => {
+  
+  const [theme , setTheme]=useState('light');
+  const toggleTheme=()=>{
+    setTheme(prevTheme=>(prevTheme)=='light'?'dark':'light');
+  };
+  
   return (
-    <ThemeContext.Provider value="dark">
+    <ThemeContext.Provider value={theme}>
       <div style={{ border: "2px solid black", padding: 20 }}>
         <h2>App (Parent)</h2>
+        <button onClick={toggleTheme}>Toggle Theme</button>
         <ComponentA />
       </div>
     </ThemeContext.Provider>
