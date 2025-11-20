2e04029c455bb34ea531b70b3f5131d2cda03573
Md Imam Hussain <mihussain737@gmail.com>
2025-11-20 20:12:55 +0530

    use Context added

--- first react app/src/components/ContextExample.jsx
+++ first react app/src/components/ContextExample.jsx
@@ -1,12 +1,40 @@
-import React, { createContext } from 'react'
+import React, { createContext, useContext } from 'react';
 
+const themeContext=createContext("light");
 const ContextExample = () => {
-     const MyContext=createContext()
   return (
-    <div>
-
+    <div style={{border:'2px solid black',padding:20}}>
+      <h2>App (Parent)</h2>
+      <ComponentA/>
     </div>
   )
 }
 
+function ComponentA(){
+  return(
+    <div style={{border:'2px solid red',padding:20}}>
+      <h2>Component A(Child)</h2>
+      <ComponentB/>
+    </div>
+  )
+}
+
+function ComponentB(){
+  return(
+    <div style={{border:'2px solid green',padding:20}}>
+      <h2>Component B(Great Child)</h2>
+      <ThemeComponent/>
+    </div>
+  )
+}
+
+function ThemeComponent(){
+  const theme =useContext(themeContext);
+  return(
+    <div style={{border:'2px solid yellow',padding:20}}>
+      <h2>ThemeComponent(Great-GrandChild)</h2>
+      <div>The Current theme is:{theme}</div>
+    </div>
+  )
+}
 export default ContextExample
