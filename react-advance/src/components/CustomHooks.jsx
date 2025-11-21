4f1d8cfb4d60004f0a92827cf2b57e874703f4e9
Md Imam Hussain <mihussain737@gmail.com>
2025-11-21 12:36:20 +0530

    custom hook updating

--- react-advance/src/components/CustomHooks.jsx
+++ react-advance/src/components/CustomHooks.jsx
@@ -3,13 +3,14 @@ import useCounter from './useCounter'
 const CustomHooks = () => {
      // Custom hooks in React are functions that let you reuse logic across multiple components.
      // Start with use
-     const {count,increment,decrement}=useCounter();
+     const {count,increment,decrement,reset}=useCounter();
 
   return (
     <div>
           <h1>Count: {count}</h1>
           <button onClick={increment}>Increment</button>
           <button onClick={decrement}>Decrement</button>
+          <button onClick={reset}>Reset</button>
     </div>
   )
 }
--- react-advance/src/components/useCounter.jsx
+++ react-advance/src/components/useCounter.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
-const useCounter = () => {
+const useCounter = (initialValue=0) => {
      const [count,setCount]=useState(0);
 
      const increment=()=>setCount(count+1);
      const decrement=()=>setCount(count-1);
 
-  return {count,increment,decrement}
+     const reset=()=> setCount(initialValue);
+
+  return {count,increment,decrement, reset}
 }
 
 export default useCounter;
