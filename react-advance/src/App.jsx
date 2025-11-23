695252d10b320d9e2c981b4ed7b206a73e8e9194
Md Imam Hussain <mihussain737@gmail.com>
2025-11-23 11:01:27 +0530

    axios api call

--- react-advance/src/App.jsx
+++ react-advance/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import CustomHooks from './components/CustomHooks'
 import ApiWork from './components/ApiWork'
+import AxiosDemo from './components/AxiosDemo'
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <div>
       {/* <CustomHooks></CustomHooks> */}
-      <ApiWork></ApiWork>
+      {/* <ApiWork></ApiWork> */}
+      <AxiosDemo></AxiosDemo>
     </div>
   )
 }
---
+++ react-advance/src/components/AxiosDemo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AxiosDemo = () => {
+  return (
+    <div>AxiosDemo</div>
+  )
+}
+
+export default AxiosDemo
