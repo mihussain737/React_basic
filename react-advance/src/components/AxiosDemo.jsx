b3a2027f75860be78d67a8f014f7d31f7f05098e
Md Imam Hussain <mihussain737@gmail.com>
2025-11-23 20:31:42 +0530

    axios demo added

--- react-advance/src/components/AxiosDemo.jsx
+++ react-advance/src/components/AxiosDemo.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect ,useState} from 'react';
+import axios from 'axios'
 
 const AxiosDemo = () => {
+  const [data,setData]=useState([]);
+  const [loading,setLoading]=useState(false);
+  const [error,setError]=useState(null);
+
+   useEffect(()=>{
+            setLoading(true);
+       axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            setData(response.data);
+            setLoading(false);
+        }).catch(error=>{
+            console.error(`Error fetching data:`,error);
+            setError('Failed to fetch the data')
+        })
+       },[]);
+
+      if(loading){
+          return <>Loading...</>
+     }
+
+     if(error){
+          return <p>{error}</p>
+     }
+
   return (
-    <div>AxiosDemo</div>
+    <div>
+     <h1>APIs</h1>
+     <ul>
+          {
+               data.map((post)=>(
+                    <li key={post.id}>
+                         <h2>{post.title}</h2>
+                         <p>{post.body}</p>
+                    </li>
+               ))
+          }
+     </ul>
+    </div>
   )
 }
 
-export default AxiosDemo
+export default AxiosDemo;
