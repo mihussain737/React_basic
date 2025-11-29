39af6d559f08a2aa99077066a3ee51aee0a4a82e
Md Imam Hussain <mihussain737@gmail.com>
2025-11-29 11:11:42 +0530

    routing setup and demo

--- react-advance/src/App.jsx
+++ react-advance/src/App.jsx
@@ -6,18 +6,29 @@ import AxiosDemo from './components/AxiosDemo'
 import PostApi from './components/PostApi'
 import ReactHook from './components/ReactHook'
 import Routing from './components/Routing'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/Home'
+import AboutUs from './components/AboutUs'
+import Contact from './components/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <div>
-      {/* <CustomHooks></CustomHooks> */}
-      {/* <ApiWork></ApiWork> */}
-      {/* <AxiosDemo></AxiosDemo> */}
-      {/* <PostApi></PostApi> */}
-      {/* <ReactHook/> */}
-      <Routing></Routing>
-    </div>
+    // <div>
+    //   <CustomHooks></CustomHooks>
+    //   <ApiWork></ApiWork>
+    //   <AxiosDemo></AxiosDemo>
+    //   <PostApi></PostApi>
+    //   <ReactHook/>
+    //   <Routing></Routing>
+    // </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<AboutUs/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ react-advance/src/components/AboutUs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div><h2>About Us</h2></div>
+  )
+}
+
+export default AboutUs
