77a3cff834604c9718e330fd065b8f36c049b533
Md Imam Hussain <mihussain737@gmail.com>
2025-12-02 21:37:35 +0530

    redux added

--- redux-react/src/App.jsx
+++ redux-react/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +8,7 @@ function App() {
   return (
     <div>
       <p>Welcome!</p>
+      <Counter></Counter>
     </div>
   )
 }
---
+++ redux-react/src/Counter.jsx
@@ -0,0 +1,27 @@
+import {useSelector,useDispatch} from 'react-redux';
+import {increment} from './store/actions/action';
+import {decrement} from './store/actions/action';
+
+/*
+     useSelector is a React-redux hook that lets your React component
+     access data from the Redux store.
+
+     useDispatch is another React-Redux hook that lets your component
+     send actions to the Redux store.
+*/
+
+const Counter = () => {
+
+     const count=useSelector(state=>state.count);
+     const dispatch=useDispatch();
+  return (
+    <div>
+          <h2>Count in counter Component: {count}</h2>
+          <button onClick={()=>dispatch(increment())}>Increment</button>
+          <br /> <br />
+          <button onClick={()=>dispatch(decrement())}>Decrement</button>
+     </div>
+  )
+}
+
+export default Counter
