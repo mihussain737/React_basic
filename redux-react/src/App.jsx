a57ae0c5c130e899960844ef0d856af56381eebd
Md Imam Hussain <mihussain737@gmail.com>
2025-12-03 22:12:05 +0530

    bug fixed

--- redux-react/src/App.jsx
+++ redux-react/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import Counter from './Counter'
 import Display from './Display'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
